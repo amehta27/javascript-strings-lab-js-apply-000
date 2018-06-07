ceee76ac1febbf37b139d35976061d21c681e0ca
Alpa Mehta <apmk_27@yahoo.com>
2018-06-07 14:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+//"Hello, "+ specialGuest;
+
+var greetSpecialGuest = "" + specialGuest + "!";
+>>>>>>> 942cc280b3a3987549ca519c27edc996f7f9c103
 
 var topic = "space";
 
